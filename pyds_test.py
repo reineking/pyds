517dbfa68f3c804a7b3369074bbd54a075f319bc
Thomas Reineking <thomas.reineking@gmail.com>
2014-10-10 15:05:47 +0200

    change directory structure switch to BSD license

--- pyds_test.py
+++ pyds_test.py
@@ -1,21 +1,3 @@
-# Copyright (C) 2010-2011  Thomas Reineking
-#
-# This file is part of the PyDS library.
-# 
-# PyDS is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# PyDS is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 """
 Unit tests for "pyds.py".
 """
